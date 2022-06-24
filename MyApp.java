ede4e49c3cf27ef90bad875666ef59c217db323e
Ashu Sharma <81765508+ashusharma890@users.noreply.github.com>
2022-06-25 02:26:11 +0530

    Created MyApp.java

---
+++ MyApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.notepad;
+
+/**
+ *
+ * @author user
+ */
+public class MyApp {
+    public static void main(String[] args)
+    {
+        Notepad notepad= new Notepad();
+    }
+}
